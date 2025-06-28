3fcb38364e33fbf7174334ce955b68b9eeb39160
Erjiao Zhou <erjiao.zhou@edu.fh-joanneum.at>
2025-06-29 00:10:42 +0200

    changed getter to computed of selectedLifter

--- goodfellas-lifts-app/src/app/lifters.ts
+++ goodfellas-lifts-app/src/app/lifters.ts
@@ -1,47 +1,47 @@
-export const DUMMY_USERS = [
+export const LIFTERS = [
   {
     id: 'u1',
     name: 'Erjiao Zhou',
-    avatar: 'user-1.png',
+    avatar: 'lifter-1.png',
   },
   {
     id: 'u2',
     name: 'Emily Thompson',
-    avatar: 'user-2.jpg',
+    avatar: 'lifter-2.jpg',
   },
   {
     id: 'u3',
     name: 'Marcus Johnson',
-    avatar: 'user-3.jpg',
+    avatar: 'lifter-3.jpg',
   },
   {
     id: 'u4',
     name: 'David Miller',
-    avatar: 'user-4.jpg',
+    avatar: 'lifter-4.jpg',
   },
   {
     id: 'u5',
     name: 'Priya Patel',
-    avatar: 'user-5.jpg',
+    avatar: 'lifter-5.jpg',
   },
   {
     id: 'u6',
     name: 'Arjun Singh',
-    avatar: 'user-6.jpg',
+    avatar: 'lifter-6.jpg',
   },
   {
     id: 'u7',
     name: 'David Endlicher',
-    avatar: 'user-7.jpg',
+    avatar: 'lifter-7.jpg',
   },
   {
     id: 'u8',
     name: 'David Poptnig',
-    avatar: 'user-8.png',
+    avatar: 'lifter-8.png',
   },
   {
     id: 'u9',
     name: 'Kamilo Knezevic',
-    avatar: 'user-9.png',
+    avatar: 'lifter-9.png',
   },
 ];
